dbc224c88b51955d5000158d33557fe90a52581f
John Ngo <jngo@users.noreply.github.com>
2025-06-05 23:49:22 +0200

    Added tabs and about content.

--- components/DiagramControls.tsx
+++ components/DiagramControls.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button"
 import { Download, ZoomIn, ZoomOut, Crosshair } from "lucide-react"
 import { RefObject } from "react"
 import { Card } from "@/components/ui/card"
+
 interface DiagramControlsProps {
   transformRef: RefObject<any>
   onExport: () => void
@@ -9,7 +10,7 @@ interface DiagramControlsProps {
 
 export function DiagramControls({ transformRef, onExport }: DiagramControlsProps) {
   return (
-    <Card className="absolute inset-x-0 bottom-0 mx-auto w-fit p-1 z-10 shadow-md flex items-center justify-center space-x-2 pointer-events-auto">
+    <Card className="absolute inset-x-0 bottom-0 mx-auto w-fit p-1 z-10 shadow-md flex items-center justify-center space-x-1 pointer-events-auto">
       <Button 
         variant="ghost"
         size="icon"
--- components/DiagramEditor.tsx
+++ components/DiagramEditor.tsx
@@ -3,6 +3,7 @@ import { Textarea } from "@/components/ui/textarea"
 import { useState } from "react"
 import { ChevronUp, ChevronDown } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 interface DiagramEditorProps {
   mermaidCode: string
@@ -18,31 +19,38 @@ export function DiagramEditor({ mermaidCode, onCodeChange, error }: DiagramEdito
   }
 
   return (
-    <Card className={`absolute inset-x-0 top-0 max-w-lg mx-auto max-h-full z-20 p-3 overflow-hidden flex flex-col shadow-md pointer-events-auto transition-all duration-200 ease-in-out ${isExpanded ? 'h-96' : 'h-12.5'}`}>
-      <div className="mb-3 flex items-center justify-between">
-        <h1 className="text-sm font-medium">Mermaid Editor</h1>
+    <Tabs defaultValue="editor" className={`absolute inset-x-0 top-0 max-w-lg mx-auto max-h-full ${isExpanded ? 'h-auto' : 'h-[2.875rem]'} rounded-lg border bg-card shadow-md px-4 pt-5 pb-4 pointer-events-auto flex flex-col items-center overflow-hidden`} onValueChange={() => setIsExpanded(true)}>
+      <div className="flex items-center justify-between w-full">
+        <TabsList className="w-min h-7 px-0.75 py-1 -mt-3 mb-2 rounded-md">
+          <TabsTrigger value="editor" className="h-[1.45rem] px-2 py-1 rounded-sm text-xs">Editor</TabsTrigger>
+          <TabsTrigger value="about" className="h-[1.45rem] px-2 py-1 rounded-sm text-xs">About</TabsTrigger>
+        </TabsList>
         <Button
           variant="ghost"
           size="icon"
-          className="-m-2"
+          className="-mr-2 -mt-5 size-7"
           onClick={toggleExpand}
         >
           {isExpanded ? <ChevronUp /> : <ChevronDown />}
         </Button>
       </div>
-      <Textarea
-        value={mermaidCode}
-        onChange={(e) => onCodeChange(e.target.value)}
-        onFocus={() => setIsExpanded(true)}
-        onBlur={() => setIsExpanded(false)}
-        placeholder="Enter Mermaid diagram code here..."
-        className="h-full min-h-10 2 font-mono text-base text-nowrap" 
-      />
-      {error && (
-        <div className="p-2 text-xs text-red-500 bg-background/95 backdrop-blur-sm supports-backdrop-filter:bg-background/60">
-          {error}
-        </div>
-      )}
-    </Card>
+
+      <TabsContent value="editor" className="w-full grow">
+        <Textarea
+          value={mermaidCode}
+          onChange={(e) => onCodeChange(e.target.value)}
+          className="h-full font-mono text-base text-nowrap h-[72dvh] max-h-[72dvh]"
+        />
+
+        {error && (
+          <p className="text-base text-red-500 mt-2">{error}</p>
+        )}
+      </TabsContent>
+
+      <TabsContent value="about" className="w-full grow text-muted-foreground text-sm space-y-2">
+        <p>I created Mermaid View because I generate a lot of Mermaid diagrams with ChatGPT and needed a way to quickly preview them on both desktop and mobile.</p>
+        <p>Mermaid is a simple and user-friendly tool that allows you to create visual diagrams and flowcharts using plain text.</p>
+      </TabsContent>
+    </Tabs>
   )
 } 
